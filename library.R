a1bc065b4986923bfd8b9fc355d55375a98cdd41
Wendy Wang <wangwenusa11@gmail.com>
2017-08-21 23:32:15 -0700

    Final touch

--- library.R
+++ library.R
@@ -1,4 +1,21 @@
+#packages we need for building a package
 install.packages("devtools")
 library("devtools")
 devtools::install_github("klutometis/roxygen")
 library(roxygen2)
+
+#create package directory
+setwd("parent_directory")
+create("cats")
+
+cat_function <- function(love=TRUE){
+  if(love==TRUE){
+    print("I love cats!")
+  }
+  else {
+    print("I am not a cool person.")
+  }
+}
+
+setwd("./cats")
+document()
